5038d304e023fc4e7ed892b9681465fe33ce97f9
Tonysiu <thefool363@gmail.com>
2019-09-17 17:48:33 +0800

    GUIstuff

---
+++ calculator.py
@@ -0,0 +1,24 @@
+operators = ['add','subtract','multiply','divide','equals']
+button = [' ','root', 0, 'expo']
+num, num2 = len(operators), len(button)
+
+for i in range(num):
+    operand = tk.Button(text=operators[i])
+    operand.grid(column=5,row=i)
+
+for i in range(1,4):
+    for m in range(1,i+1):
+        text = tk.Label(text="#")
+        text.grid(column=m,row=0)
+    for l in range(1, num2):
+        accessory = tk.Button(text=button[l])
+        accessory.grid(column=l, row=4)
+    for j in range(1,i+1):
+        label2 = tk.Button(text=j + 3)
+        label2.grid(column=j, row=2)
+    for k in range(1,i+1):
+        label3 = tk.Button(text=k + 6)
+        label3.grid(column=k, row=3)
+    label = tk.Button(text=i)
+    label.grid(column=i, row=1)
+window.mainloop()
