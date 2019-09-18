221fe986b3c217447d5345c3140f984b5f3a25a0
Tonysiu <thefool363@gmail.com>
2019-09-18 17:27:04 +0800

    data binding

--- GUI_Development.py
+++ GUI_Development.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 # from tkinter import ttk
 
+def doorbell(event):
+    print("you rang the doorbell!")
 window = tk.Tk()
 window.geometry("300x200")
 
@@ -10,9 +12,12 @@ alabel.grid(column=0,row=0)
 blabel = tk.Label(text="Apple")
 blabel.grid(column= 1, row=0)
 
-button = tk.Button(text="5")
+button = tk.Button(window, text="doorbell")
 button.grid(column=0, row=1)
 
-button2 = tk.Button(text="10")
+button2 = tk.Button(window, text="10")
 button2.grid(column=1, row=1)
+
+button.bind("<Button-1>", doorbell)
+
 window.mainloop()
--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
-operators = ['add','subtract','multiply','divide','equals']
-button = [' ','root', 0, 'expo']
+import tkinter as tk
+
+
+window = tk.Tk()
+window.geometry("300x200")
+
+operators = ['+','-','*','/','=']
+button = [' ','rt', 0, '**']
 num, num2 = len(operators), len(button)
 
 for i in range(num):
@@ -11,14 +17,14 @@ for i in range(1,4):
         text = tk.Label(text="#")
         text.grid(column=m,row=0)
     for l in range(1, num2):
-        accessory = tk.Button(text=button[l])
+        accessory = tk.Button(text="   " + str(button[l]) + "   ")
         accessory.grid(column=l, row=4)
     for j in range(1,i+1):
-        label2 = tk.Button(text=j + 3)
+        label2 = tk.Button(text="   " + str(j + 3) + "   ")
         label2.grid(column=j, row=2)
     for k in range(1,i+1):
-        label3 = tk.Button(text=k + 6)
+        label3 = tk.Button(text="   " + str(k + 6) + "   ")
         label3.grid(column=k, row=3)
-    label = tk.Button(text=i)
+    label = tk.Button(text="   " + str(i) + "   ")
     label.grid(column=i, row=1)
 window.mainloop()
