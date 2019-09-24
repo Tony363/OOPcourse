32c30b7eb3f22368929e53fb6a1aaf19aa9dcaa0
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-09-24 23:42:04 +0800

    calling it a night

--- Bookmarks.py
+++ Bookmarks.py
@@ -7,11 +7,6 @@ from tkinter import *
 window = tk.Tk()
 window.geometry("400x200")
 
-
-
-global Button_Count
-Button_Count = 0
-
 entry_id = StringVar() # create an id for your entry, this helps getting the text
 entry = tk.Entry(window, textvariable=entry_id, justify=RIGHT)
 entry.grid(row=0, column=1, sticky=E)
@@ -26,15 +21,12 @@ def addBookmark(event):
     Bookmark = StringVar()
     invisible = tk.Button(window, text='', textvariable=Bookmark)
     invisible.grid(row=1, column=1, sticky=W)
-    reply = Bookmark.set(format(entry_id)) # format the text on the invisible label you created above
-		
-    def clickcount():
-        global Button_Count
-        Button_Count += 1
+     
+    def clicked():
+	    text = entry_id.get() # get the text from entry
+		reply = Bookmark.set(format(text)) # format the text on the invisible label you created above
+		return reply
     
-    # Bookmark.grid(column=0,row=Button_Count)
-    return reply
- 
 
 add_bookmark = tk.Button(window,text="Add Bookmark")
 add_bookmark.grid(column=5,row=0)
