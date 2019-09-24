78403a10a5858a9f5f974aa38f58906fca8ca8cd
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-09-24 16:38:23 +0800

    cont.

--- GUI_Development.py
+++ GUI_Development.py
@@ -1,8 +1,16 @@
 import tkinter as tk
+import webbrowser
 # from tkinter import ttk
 
 def doorbell(event):
     print("you rang the doorbell!")
+
+def fuckyou(event):
+    print("fuckyou")
+
+def link(event):
+    webbrowser.open_new_tab("www.google.com")
+    
 window = tk.Tk()
 window.geometry("300x200")
 
@@ -14,10 +22,14 @@ blabel.grid(column= 1, row=0)
 
 button = tk.Button(window, text="doorbell")
 button.grid(column=0, row=1)
+button.bind("<Button-1>", doorbell)
 
 button2 = tk.Button(window, text="10")
 button2.grid(column=1, row=1)
+button2.bind("<Button-1>", fuckyou)
 
-button.bind("<Button-1>", doorbell)
+button3 = tk.Button(text="google.com")
+button3.grid(column=2, row=1)
+button3.bind("<Button-1>", link)
 
 window.mainloop()
