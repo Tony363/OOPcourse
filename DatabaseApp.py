5b8b722dc1f6922b7eb923da776eb35e74bc6483
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-09-26 23:48:40 +0800

    gaga, i don't use branches much

--- DatabaseApp.py
+++ DatabaseApp.py
@@ -6,20 +6,78 @@ root.title("My Bookmarks!")
 root.geometry("400x400")
 
 # create a datase or connect to one
-conn = sqlite3.connect('book_marks.db')
+conn = sqlite3.connect('bookmarks.db')
 
 # create cursor
 cursor = conn.cursor()
 
 #create table
-cursor.execute("""CREATE TABLE book_marks (
-        first_name text,
-        last_name text,
-        address text,
-        city text,
-        state text,
-        zipcode integer
-        )""")
+cursor.execute("""CREATE TABLE bookmarks (textbox text)""")
+
+def links():
+    # create a datase or connect to one
+    conn = sqlite3.connect('bookmarks.db')
+
+    # create cursor
+    cursor = conn.cursor()
+
+    #Query database
+    cursor.execute("SELECT *, oid FROM bookmarks")
+    marks = cursor.fetchall()
+    print(marks)
+
+    for mark in marks[0]:
+        print_marks += mark
+
+
+    # Commit Changes
+    conn.commit()
+
+    # close connection
+    conn.close()
+
+    
+
+#create submit function for database
+def submit():
+    # create a datase or connect to one
+    conn = sqlite3.connect('bookmarks.db')
+
+    # create cursor
+    cursor = conn.cursor()
+
+    #insert into table
+    cursor.execute("INSERT INTO bookmarks VALUES (:textbox )",
+                    {
+                        'textbox': textbox.get()
+                    }
+                    )
+
+    # Commit Changes
+    conn.commit()
+
+    # close connection
+    conn.close()
+    
+
+
+
+
+textbox = Entry(root, width=30)
+textbox.grid(row=0, column=1, padx=20)
+
+#Craete text box Labels
+textLabel = Label(root, text="enter link")
+textLabel.grid(row=0, column=0)
+
+#create button for text box
+
+sbt = Button(root, text="Add Book Marks!", command=submit)
+sbt.grid(row=0, column=2, pady=10, padx=10)
+
+#create a link button
+linkbtn = Button(root, text="links", command=links)
+linkbtn.grid(row=1, column=0, columnspan=4, pady=10, padx=10, ipadx=137)
 
 #Commit Changes
 conn.commit()
