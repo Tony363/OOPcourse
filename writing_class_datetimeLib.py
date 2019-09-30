f8858190007219697d463a03ecdc1d81eda51a58
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-09-30 19:44:35 +0800

    qazis version of age calculator app

--- writing_class_datetimeLib.py
+++ writing_class_datetimeLib.py
@@ -1,6 +1,6 @@
 import datetime
+import tkinter as tk
 from datetime import date
-from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 # rdelta = relativedelta(date.today(),datetime.date(1996,5,6))
@@ -8,6 +8,37 @@ from dateutil.relativedelta import relativedelta
 
 # datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
 # print(datetime_object)
+
+#create frame
+window = tk.Tk()
+
+#create frame geometry
+window.geometry("400x400")
+
+#set the title of the frame
+window.title("Age Calculator APP")
+
+#adding labels
+year_label = tk.Label(text="Year")
+year_label.grid(column=0, row=1)
+
+month_label = tk.Label(text="Month")
+month_label.grid(column=0, row=2)
+
+day_label = tk.Label(text="Day")
+day_label.grid(column=0, row=3)
+
+year_entry = tk.Entry()
+year_entry.grid(column=1, row=1)
+
+month_entry = tk.Entry()
+month_entry.grid(column=1, row=2)
+
+day_entry = tk.Entry()
+day_entry.grid(column=1, row=3)
+
+calculate_button = tk.Button(window,text='Calculate Now')
+calculate_button.grid(column=1, row=4)
 
 class Person:
     def __init__(self, name, birthdate):
@@ -25,10 +56,13 @@ class Person:
     def __str__(self):
         return "{}".format(age)
 
+trial = date.today() - datetime.date(1996,5,6)
+print(trial)
 
 Tony = Person("Tony", datetime.date(1996,5,6))
 print(Tony.name + ":" + str(Tony.birthdate) + "," + "Age being " + str(Tony.age()))
 # print(Tony.age())
 
-# trial = date.today() - datetime.date(1996,5,6)
-# print(trial)
+
+
+window.mainloop()
