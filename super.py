8aecdb5d8d27586e20a3014513c0b41c5c0bfb0b
Tony <pysolver33@gmail.com>
2019-10-31 17:17:49 +0800

    super inheritence

--- super.py
+++ super.py
@@ -11,12 +11,54 @@ class Rectangle:
 
 class Square(Rectangle):
     def __init__(self, length):
-        super(Square, self).__init__(length, length)
+        super(Square,self).__init__(length,length)
 
 rec = Rectangle(16,4)
 print(rec.area())
 print(rec.perimeter())
 
+
 box = Square(5)
 print(box.area())
 print(box.perimeter())
+
+class Column(Rectangle):
+    def __init__(self,length, width):
+        super(Column,self).__init__(length,width)
+        Area = super(Column,self).area()
+
+    def surface_area(self):
+         
+        return super(Column,self).area() * 6
+
+    def volume(self):
+       
+        return super(Column,self).area() * self.length
+    
+    # def __int(self):
+    #     return 
+
+boxy = Column(16,4)
+print(boxy.surface_area())
+print(boxy.volume())
+
+class A:
+    def test(self):
+        print("test of A called")
+
+class B(A):
+    def test(self):
+        print('test of B called')
+        super().test()
+
+class C(A):
+    def test(self):
+        print("test of C called")
+        super().test()
+class D(B,C):
+    def test2(self):
+        print("test of D called")
+
+obj = D()
+print(obj.test())
+# print(obj.test2())        
