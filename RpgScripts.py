c030e4a1156648e5898239d08bfff63d1f34c7ec
tony363 <pysolver33@gmail.com>
2020-03-25 17:57:53 +0800

    little game for next lesson

--- RpgScripts.py
+++ RpgScripts.py
@@ -5,22 +5,44 @@ class Fighter:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.damage = random.randint(1,20)
-        # self.current_health = self.health - self.damage
 
-    def attack(self, other_guy):
-        other_guy.health = other_guy.health - self.damage
-        print("{} attacks {}".format(self.name, other_guy.name))
     def __str__(self):
-        return "{}: {}".format(self.name, self.health)
+        return "{}: health {}".format(self.name, self.health)
+
+    def attack(self):
+        self.damage = random.randint(1,20)
+        
+    def update_health(self,other_guy):
+        self.health = self.health - other_guy.damage
+        return f'{self.name} health is {self.health}'
 
 Gus = Fighter("Gus")
 Joey = Fighter("Joey")
 
-print(Gus)
-print(Joey)
+player_list = [Gus,Joey]
+ user = str(input('continue?[yes/no]'))
+    if user == 'no':
+        break
+while True:
+    for player in player_list:
+        player.attack()
+        if player.health < 0:
+            player_list.remove(player)
+    
+    user = str(input('continue?[yes/no]'))
+    if user == 'no':
+        break
+            
 
-Joey.attack(Gus)
-print("Gus takes " + str(Joey.damage) + " points of damage!")
-
-print("Gus has "  + str(Gus.health) + " health points left!")
+    if len(player_list) == 1:
+        print(f'{player_list[0]} has won!')
+        break
+        
+                
+    player_list[0].update_health(player_list[1])
+    player_list[1].update_health(player_list[0])
+    print("Gus takes " + str(Joey.damage) + " points of damage!")
+    print("Gus has "  + str(Gus.health) + " health points left!")
+    print("Joey takes " + str(Gus.damage) + " points of damage!")
+    print("Gus has "  + str(Joey.health) + " health points left!")
+  
